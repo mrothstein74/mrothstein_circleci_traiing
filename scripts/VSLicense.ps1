b38194869743afe68ad4d86121d096490cfaa13e
mrothstein74 <michael.rothstein@circleci.com>
2021-10-28 16:41:59 -0700

    adding config

--- scripts/VSLicense.ps1
+++ scripts/VSLicense.ps1
@@ -2,7 +2,14 @@
 
 Import-Module -Name 'C:\Users\circleci\project\VSCELicense\VSCELicense.psd1'
 
+Write-Output "Before update"
 Get-VSCELicenseExpirationDate -Version 2019
 
+# Set the timeout to 31 days
 Set-VSCELicenseExpirationDate -Version 2019 -AddDays 31
+
+Write-Output "After update"
 Get-VSCELicenseExpirationDate -Version 2019
+
+# Delete the scheduled task
+Unregister-ScheduledTask -TaskName "ccitimeout" -Confirm:$false
